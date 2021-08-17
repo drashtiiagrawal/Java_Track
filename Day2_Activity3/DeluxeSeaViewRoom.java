a6e8fc9600b1b58a8adb8ac8be5cd04f5286bd63
drashtiiagrawal <88571105+drashtiiagrawal@users.noreply.github.com>
2021-08-17 09:29:19 +0530

    Add files via upload

---
+++ Day2_Activity3/DeluxeSeaViewRoom.java
@@ -0,0 +1,20 @@
+package mypackage;
+
+class DeluxeSeaViewRoom extends DeluxeRoom{
+
+	/**
+	 * @param hotelName
+	 * @param numberOfSqFeet
+	 * @param hasTV
+	 * @param hasWifi
+	 */
+	public DeluxeSeaViewRoom(String hotelName, int numberOfSqFeet, boolean hasTV, boolean hasWifi) {
+		super(hotelName, numberOfSqFeet, hasTV, hasWifi);
+		// TODO Auto-generated constructor stub
+		this.ratePerSqFeet=12;
+	}
+	
+	
+	
+
+}
