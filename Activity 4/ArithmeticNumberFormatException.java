a47dca34bdb03c09b7514581a2e38e2e498da65e
drashtiiagrawal <88571105+drashtiiagrawal@users.noreply.github.com>
2021-08-17 09:31:03 +0530

    Add files via upload

---
+++ Activity 4/ArithmeticNumberFormatException.java
@@ -0,0 +1,32 @@
+package mypackage;
+
+import java.io.*;
+
+class ArithmeticNumberFormatException {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int runsScored, oversFaced;
+		float runRate;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the total runs scored");
+		try {
+			runsScored = Integer.parseInt(br.readLine());
+			System.out.println("Enter the total overs faced");
+			oversFaced = Integer.parseInt(br.readLine());
+			runRate = (float)runsScored / oversFaced;
+			System.out.printf("Current Run Rate :%.2f",runRate);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getClass());
+		}
+		
+		
+
+	}
+
+}
